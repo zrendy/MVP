2559a8cb24fdd7963ab8f4556ba4f8e0538015af
zrendy <zrendy@gmail.com>
2022-11-16 18:48:38 -0500

    added port

--- server/server.js
+++ server/server.js
@@ -4,9 +4,8 @@ const path = require('path');
 require('dotenv').config()
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const {postPhotos, getPhotos} = require('./database/controllers/photoStore.js')
-
 
 //for use in development
 app.use(cors());
